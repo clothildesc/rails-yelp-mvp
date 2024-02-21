afe483f700e790ed5ccae069a88871f4f28139ef
clothilde scache <clothilde.sca@gmail.com>
2024-02-21 15:21:38 +0100

    created app yelp

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,18 @@
+class ReviewsController < ApplicationController
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = Restaurant.find(params[:restaurant_id])
+    if @review.save
+      redirect_to @review.restaurant, notice: 'Review was successfully created.'
+    else
+      render 'restaurants/show', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,17 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+
+addomme = { name: 'Addomme Pizzeria', address: '41 rue des Tournelles 75003 Paris', phone_number: '0762883766', category: 'italian'}
+oxymore = { name: 'Oxymore', address: '60 rue Saint-Maur 75011 Paris', phone_number: '0147007324', category: 'italian'}
+le_camion_qui_fume = { name: 'Le Camion Qui Fume', address: '66 rue Oberkampf 75011 Paris', phone_number: '0984330469', category: 'french'}
+come_prima = { name: 'Come Prima', address: '33 rue Godot de Mauroy 75009 Paris', phone_number: '0147422337', category: 'italian'}
+ober_mamma = { name: 'Ober Mamma', address: '107 bd Richard-Lenoir 75011 Paris', phone_number: '0186477834', category: 'french'}
+
+[addomme, oxymore, le_camion_qui_fume, come_prima, ober_mamma].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+
+puts 'Finished!'
